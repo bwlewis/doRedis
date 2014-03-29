0fa17f4c0b563b6bbdff649a18354c520bf3d07a
B. W. Lewis <blewis@illposed.net>
2014-03-29 14:25:35 -0400

    Added backward-support for older pre-Lua versions of redis

--- R/task.R
+++ R/task.R
@@ -5,7 +5,7 @@
 # queue (characer) name of job queue
 # job_id (character) job ID
 #
-# The function must return a task list, an R list with the following
+# The function must return null or a task list, an R list with the following
 # fields:
 # task_id (character) task ID
 # args (list) expression arguments
@@ -13,5 +13,24 @@
 default_getTask <-  function(queue, job_id, ...)
 {
   key <- sprintf("%s:%s",queue, job_id)
-  redisEval("local x=redis.call('hkeys',KEYS[1])[1];if x==nil then return nil end;local ans=redis.call('hget',KEYS[1],x);redis.call('hdel',KEYS[1],x);return ans",key)
-} 
+  if(version_check())
+  {
+    return(redisEval("local x=redis.call('hkeys',KEYS[1])[1];if x==nil then return nil end;local ans=redis.call('hget',KEYS[1],x);redis.call('hdel',KEYS[1],x);return ans",key))
+  }
+# We've got an old version of Redis without Lua. Manually pull a task from the
+# task hash. It's really a hack just to support old versions of Redis. Use the
+# new approach if possible.
+  tasks <- redisHKeys(key)
+  if(is.null(tasks)) return()
+  while(length(tasks)>0)
+  {
+    redisMulti()
+    redisHGet(key, tasks[[1]])
+    redisHDel(key, tasks[[1]])
+    ans <- redisExec()[[1]]
+    if(!is.null(ans)) return(ans)
+# Damn, somebody else grabbed this task before us.
+    tasks <- tasks[-1]
+  }
+  NULL
+}
