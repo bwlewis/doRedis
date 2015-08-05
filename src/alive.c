8f762f53a7a57f5c94e84440273ebbef04d3e415
B. W. Lewis <blewis@illposed.net>
2015-08-05 16:13:00 -0400

    Copied copyright notice to source code

--- src/alive.c
+++ src/alive.c
@@ -1,4 +1,23 @@
 /*
+ * Copyright (c) 2010 by Bryan W. Lewis.
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ */
+
+/*
  * alive.c: a simple fault detection system for doRedis.  doRedis worker fault
  * tolerance works by assigning two keys to each in process task: a permanent
  * key created when the task starts, and an ephemeral key kept alive by this
@@ -129,7 +148,7 @@ void *ok(void *x)
   snprintf(expire,BS,"*3\r\n$6\r\nEXPIRE\r\n$%d\r\n%s\r\n$1\r\n5\r\n", k, key);
   m = 0;
 // Check for thread termination every 1/10 sec, but only update Redis
-// every 3s.
+// every 3s (expire after 5).
   while(go>0){
     m += 1;
     if(m>30) { 
