1ec15b9dc504739e55b994b0958ec5f2d4e157a3
B. W. Lewis <blewis@illposed.net>
2015-08-07 09:29:10 -0400

    A few more tests

--- tests/test.R
+++ tests/test.R
@@ -1,11 +1,28 @@
 require(doRedis)
 
+compare = function(x,y,label="unexpected result")
+{
+  if(!isTRUE(all.equal(x,y))) stop(label)
+}
+
 if(Sys.getenv("TEST_DOREDIS")=="TRUE")
 {
+
+# Basic test with two local worker processes
   queue = "jobs"
   startLocalWorkers(n=2, queue, timeout=5)
   registerDoRedis(queue)
   ans = foreach(j=1:10,.combine=sum) %dopar% j
+  compare(ans,55, "foreach")
+
+# setX tests
+  setChunkSize(5)
+  x = 0
+  setExport("x")
+  setPackages("rredis")
+  ans = foreach(j=1:10,.combine=sum,.noexport="x") %dopar% {j + x}
+  compare(ans,55, "foreach")
+
+# Shut down
   removeQueue(queue)
-  if(!isTRUE(ans==55)) stop("incorrect result")
 }
