f679f984582a19622a38e3a0926d9d8835829f2b
B. W. Lewis <blewis@illposed.net>
2015-08-06 20:24:01 -0400

    Example test

--- tests/test.R
+++ tests/test.R
@@ -1,2 +1,11 @@
 require(doRedis)
-redisConnect()
+
+if(Sys.getenv("TEST_DOREDIS")!="TRUE")
+{
+  queue = "jobs"
+  startLocalWorkers(n=2, queue, timeout=5)
+  registerDoRedis(queue)
+  ans = foreach(j=1:10,.combine=sum) %dopar% j
+  removeQueue(queue)
+  if(!isTRUE(ans==55)) stop("incorrect result")
+}
